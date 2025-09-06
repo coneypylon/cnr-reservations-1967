c493100ba8019bae1661e0224ed0511cef055121
coneypylon <coneypylon@gmail.com>
2025-09-06 19:49:02 +0000

    added closeouts

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ ps = config['DEFAULT']['ps']
 # set up the db connection
 conn = psycopg2.connect("dbname=%s user=%s password=%s" % (db,user,ps))
 
+# custom exceptions
+class LegClosed(Exception):
+	pass
+
 def doy2monthdate(year,day):
 	date_string = "%s %s" % (year,day)
 	date_object = datetime.strptime(date_string, '%Y %j')
@@ -31,9 +35,9 @@ def pad(num,spaces):
 
 def findcars(legid,accomtype,curs):
 	if accomtype != 'A':
-		findcarsq = "SELECT remcap, carid, id, trainid FROM cardatetrain WHERE legid=%s AND accomtype='%s';" % (legid, accomtype)
+		findcarsq = "SELECT remcap, carid, id, trainid, closed FROM cardatetrain WHERE legid=%s AND accomtype='%s';" % (legid, accomtype)
 	else:
-		findcarsq = "SELECT remcap, carid, id, trainid FROM cardatetrain WHERE legid=%s;" % (legid)
+		findcarsq = "SELECT remcap, carid, id, trainid, closed FROM cardatetrain WHERE legid=%s;" % (legid)
 	curs.execute(findcarsq)
 	foundcars = curs.fetchall()
 	return foundcars
@@ -49,9 +53,9 @@ def fetchcar(legid,carcode,accomtype,curs,remcap=True):
 			else:
 				raise NotImplementedError
 			if carcode == '99':
-				outlst.append((usage,car[1],car[2],legid))
+				outlst.append((usage,car[1],car[2],legid,car[4]))
 			elif car[1].endswith(carcode):
-				outlst.append((usage,car[1],car[2],legid))
+				outlst.append((usage,car[1],car[2],legid,car[4]))
 		return outlst
 	else:
 		raise NotImplementedError
@@ -94,7 +98,7 @@ def findroutelegs(startleg,endleg,curs,westbound=True):
 		else:
 			sort = "DESC"
 			comp = ">="
-		findorderedq = '''SELECT legid 
+		findorderedq = '''SELECT legid, closed 
 			FROM legedgeindex 
 			WHERE %s %s startindex 
 			AND endindex %s %s 
@@ -105,15 +109,35 @@ def findroutelegs(startleg,endleg,curs,westbound=True):
 		legs = curs.fetchall()
 		outlegs = []
 		for leg in legs:
-			outlegs.append(leg[0])
+			outlegs.append(leg)
 		return outlegs
+
+def closeleg(legid,curs):
+	closeq = "UPDATE legs SET closed = true WHERE id = %s" % legid
+	curs.execute(closeq)
+	return 0
+
+def close(startcity,endcity,trainid,date,curs,year=1967):
+	ts = timestmp()
+	tdiff = int(ts[0:3]) - int(date)
+	if tdiff > 7 or tdiff < 0:
+		return (1,"INVDTE")
+	westbound = getdirection(startcity,endcity,curs)
+	extremelegs = findextremelegs(startcity,endcity,trainid,date,westbound,curs)
+	legs = findroutelegs(extremelegs[0][0],extremelegs[1][0],curs,westbound=westbound)
+	dt = doy2monthdate(year,date)
+	for leg in legs:
+		closeleg(leg[0],curs)
+	outstr = "XX%s %s %s" % (trainid,dt, ts)
+	return (0,outstr)
+
 
 def findextremelegs(startcity,endcity,trainid,date,westbound,curs):
 	if westbound:
 		sortdir = "ASC"
 	else:
 		sortdir = "DESC"
-	findstartlegq = '''SELECT legid, date, startcity, endcity, startindex, dayoftrain 
+	findstartlegq = '''SELECT legid, date, startcity, endcity, startindex, dayoftrain
 			FROM legedgeindex 
 			WHERE startcity='%s' 
 			AND trainid='%s' 
@@ -123,7 +147,7 @@ def findextremelegs(startcity,endcity,trainid,date,westbound,curs):
 	curs.execute(findstartlegq)
 	foundlegs = [curs.fetchone()]
 	dayoftrain = foundlegs[0][5]
-	findendlegq = '''SELECT legid, date, startcity, endcity, startindex 
+	findendlegq = '''SELECT legid, date, startcity, endcity, startindex
 			FROM legedgeindex 
 			WHERE endcity='%s' 
 			AND trainid='%s' 
@@ -163,14 +187,15 @@ def getcaps(startcity,endcity,trainid,date,carcode,accomreq,curs):
 		startleg = foundlegs[0][0]
 		endleg = foundlegs[1][0]
 		legs = findroutelegs(startleg,endleg,curs,westbound)
-		for legid in legs:
+		for leg in legs:
+			legid = leg[0]
 			car = queryleg(legid,carcode,accomreq,curs)
 			cars.extend(car)
 	for car in cars: # car is remaining car capacity, carid, cardateid but also legid
 		try:
-			carlegs[car[1]].append((car[3],car[0]))
+			carlegs[car[1]].append((car[3],car[0],car[4]))
 		except KeyError:
-			carlegs[car[1]] = [(car[3],car[0])]
+			carlegs[car[1]] = [(car[3],car[0],car[4])]
 		carid = car[1]
 		carcap = car[0]
 		cdid = car[2]
@@ -220,7 +245,7 @@ def findlegbyedge(edge,trainid,date,curs):
 	return legs
 
 def checkormakelegs(edges,trainid,date,curs):
-	stublegq = "INSERT INTO legs(trainid,edgeid,date,dayoftrain) VALUES "
+	stublegq = "INSERT INTO legs(trainid,edgeid,date,dayoftrain,closed) VALUES "
 	legs = []
 	tomake = 0
 	for edge in edges:
@@ -229,7 +254,7 @@ def checkormakelegs(edges,trainid,date,curs):
 		convdate = pad(idate + dayoftrain,2)
 		leg = findlegbyedge(edge,trainid,convdate,curs)
 		if len(leg) == 0: # no legs 
-			stublegq += "('%s',%s,'%s',0)," % (trainid,edge,date)
+			stublegq += "('%s',%s,'%s',0,false)," % (trainid,edge,date)
 			tomake += 1
 		elif len(leg) == 1: # we expect this
 			legs.append(leg[0][0])
@@ -273,13 +298,13 @@ def equip(startcity,endcity, trainid, carcode, accomtype, seats, date, curs,year
 			return (3,str(e))
 
 def trainman(startcity,endcity,trainid,date,curs,year=1967,westbound=True):
-	mincap, legs, carlegs = getcaps(startcity,endcity,trainid,date,westbound,'99','A',curs)
+	mincap, legs, carlegs = getcaps(startcity,endcity,trainid,date,'99','A',curs)
 	if len(carlegs) > 10:
 		return (1,"INVTO")
 	legnames = []
 	outstr = '     '
 	for leg in range(0,len(legs)):
-		names = getcities(legs[leg],curs)
+		names = getcities(legs[leg][0],curs)
 		outstr += names[0]
 		outstr += "  "
 		if leg == len(legs)-1:
@@ -290,7 +315,10 @@ def trainman(startcity,endcity,trainid,date,curs,year=1967,westbound=True):
 		outstr += "   "
 		for leg in carlegs[car]:
 			outstr += pad(str(leg[1]),2)
-			outstr += "   " # eventuallly sometimes a slash
+			if not leg[2]: # if the leg isn't closed
+				outstr += "   "
+			else: # it is
+				outstr += "/  "
 		outstr += "\n"
 	return (0,outstr[:-1])
 
@@ -311,9 +339,12 @@ def query(carcode,trainid,date,startcity,endcity,reqseats,accomreq,curs,year=196
 		return (carfound,mincap)
 
 def findspecificcardates(legid,carid,curs):
-	grabq = "SELECT id FROM cardates WHERE legid=%s AND carid='%s';" % (legid,carid)
+	grabq = "SELECT id, closed FROM cardatetrain WHERE legid=%s AND carid='%s';" % (legid,carid)
 	curs.execute(grabq)
-	return curs.fetchone()[0]
+	cardate = curs.fetchone()
+	if cardate[1]:
+		raise LegClosed(str(cardate[0]))
+	return cardate[0]
 
 def makelegreservation(seats,cardateid,legid,curs):
 	updateq = "UPDATE CARDATES SET remcap = remcap - %s WHERE id = %s AND legid = %s;" % (seats,cardateid,legid)
@@ -329,15 +360,16 @@ def restorinv(seats,carcode,legid,curs):
 def reserve(carid,legs,seats,date,curs,year=1967):
 	try:
 		for leg in legs:
-			cardate = findspecificcardates(leg,carid,curs)
-			makelegreservation(seats,cardate,leg,curs)
+			cardate = findspecificcardates(leg[0],carid,curs)
+			makelegreservation(seats,cardate,leg[0],curs)
 		ts = timestmp()
 		dt = doy2monthdate(year,date)
 		outstr = "OK%s %s %s" % (carid,dt, ts)
 		return (0,outstr)
+	except LegClosed:
+		return(1,"INVDTE")
 	except Exception as e:
-		print(str(e))
-		return (1)
+		return (2,str(e))
 
 if __name__ == "__main__":
 	cur = conn.cursor()
@@ -385,4 +417,9 @@ if __name__ == "__main__":
 		if requesttype == "T":
 			manifest = trainman(startcity,endcity,trainid,date,cur)
 			print(manifest[1])
+		elif requesttype == "X":
+			closeout = close(startcity,endcity,trainid,date,cur)
+			if closeout[0] == 0:
+				conn.commit()
+			print(closeout[1])
 
