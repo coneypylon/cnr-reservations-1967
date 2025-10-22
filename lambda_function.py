81705f43cbcd96f0c3640cf11ce6db014fee09e2
Nicholas Boyko <coneypylon@gmail.com>
2025-10-21 22:04:02 -0400

    Update lambda_function.py to support CORS

--- lambda_function.py
+++ lambda_function.py
@@ -433,6 +433,11 @@ def lambda_handler(event, context): # we are in a lambda
 	response = parse_n_route_string(request,cur)
 	return {
 		'statusCode': 200,
+		"headers": {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+    	},
 		'body': json.dumps(response)
 	}
 
